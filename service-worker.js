b3b30409b1f3fd519425aed9c2ab868bbf4d4f80
Phi Verbiest <113350002+PhiVerbiest@users.noreply.github.com>
2024-09-17 13:50:02 +0200

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,48 @@
+const CACHE_NAME = 'pwa-cache-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/manifest.json',
+    '/icon-192x192.png',
+    '/icon-512x512.png'
+];
+
+// Installeer de Service Worker
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+        .then(function(cache) {
+            console.log('Bestanden worden gecached');
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+// Haal bestanden op uit de cache
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+        .then(function(response) {
+            if (response) {
+                return response; // Geef gecachte versie terug
+            }
+            return fetch(event.request); // Haal van het netwerk
+        })
+    );
+});
+
+// Verwijder oude caches bij activatie van een nieuwe Service Worker
+self.addEventListener('activate', function(event) {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.map(function(cacheName) {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
